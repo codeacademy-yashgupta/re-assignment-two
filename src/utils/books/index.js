365d6f2cc98f671abee4e4196e0d4c14871e2fda
Yash <guptayash955@gmail.com>
2019-02-07 16:34:19 +0530

    added get books rating by id from external api

--- src/utils/books/index.js
+++ src/utils/books/index.js
@@ -5,4 +5,10 @@ const getBooksWithoutRating = () => {
   const externalAPIForGettingBooksWithoutRating = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
   return httpGet(externalAPIForGettingBooksWithoutRating);
 };
-module.exports = { getBooksWithoutRating };
+const getBookRatingById = (id) => {
+  const externalAPITwoForGettingRating = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
+  return httpGet(externalAPITwoForGettingRating);
+};
+
+
+module.exports = { getBooksWithoutRating, getBookRatingById };
--- tests/utils/books/index.test.js
+++ tests/utils/books/index.test.js
@@ -2,11 +2,18 @@
  * @jest-environment node
  */
 
-const { getBooksWithoutRating } = require('./../../../src/utils/books');
+const { getBooksWithoutRating, getBookRatingById } = require('./../../../src/utils/books');
 
 describe('getBooksWithoutRating', () => {
   it('should fetch all the books from external API', async () => {
     const books = await getBooksWithoutRating();
     expect(Array.isArray(books.data.books)).toEqual(true);
+  });
+});
+
+describe('getBookRatingById', () => {
+  it('should fetch rating of the particular book id', async () => {
+    const rating = await getBookRatingById(10);
+    expect(typeof (rating.data)).toEqual('object');
   });
 });

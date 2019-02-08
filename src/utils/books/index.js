5d1d121bb5b5f53de1d7e94e8f55d81b3a3a4eaf
Yash <guptayash955@gmail.com>
2019-02-08 10:10:38 +0530

    adding get books with rating functionality

--- src/utils/books/index.js
+++ src/utils/books/index.js
@@ -1,7 +1,6 @@
 const { httpGet } = require('../http/httpGet');
 
 const getBooksWithoutRating = () => {
-  console.log('Hi');
   const externalAPIForGettingBooksWithoutRating = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
   return httpGet(externalAPIForGettingBooksWithoutRating);
 };
@@ -9,6 +8,21 @@ const getBookRatingById = (id) => {
   const externalAPITwoForGettingRating = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
   return httpGet(externalAPITwoForGettingRating);
 };
+const getBooksWithRating = async () => {
+  const books = await getBooksWithoutRating();
+  const booksData = books.data.books;
+  const ratingArrayPromise = [];
+  booksData.forEach((element) => {
+    ratingArrayPromise.push(getBookRatingById(element.id));
+  });
+  return Promise.all(ratingArrayPromise).then(allRating => allRating.map((Rating, index) => {
+    booksData[index].rating = Rating.data.rating;
+    // console.log(booksData);
+    return booksData[index];
+  }));
+};
 
 
-module.exports = { getBooksWithoutRating, getBookRatingById };
+module.exports = {
+  getBooksWithoutRating, getBookRatingById, getBooksWithRating,
+};
--- tests/utils/books/index.test.js
+++ tests/utils/books/index.test.js
@@ -2,7 +2,9 @@
  * @jest-environment node
  */
 
-const { getBooksWithoutRating, getBookRatingById } = require('./../../../src/utils/books');
+const {
+  getBooksWithoutRating, getBookRatingById, getBooksWithRating,
+} = require('./../../../src/utils/books');
 
 describe('getBooksWithoutRating', () => {
   it('should fetch all the books from external API', async () => {
@@ -15,5 +17,13 @@ describe('getBookRatingById', () => {
   it('should fetch rating of the particular book id', async () => {
     const rating = await getBookRatingById(10);
     expect(typeof (rating.data)).toEqual('object');
+  });
+});
+
+
+describe('getBooksWithRating', () => {
+  it('should return books with rating', async () => {
+    const booksWithRating = await getBooksWithRating();
+    expect(typeof (booksWithRating[0].rating)).toEqual('number');
   });
 });
